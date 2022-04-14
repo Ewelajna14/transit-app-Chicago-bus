7d06b1f3d15cbeb827e960750422ad583b71da8e
Ewelina <ekowalczyk1994@gmail.com>
2022-04-13 20:39:48 -0500

    new data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+
+  resources :stops, only: [:index]
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
