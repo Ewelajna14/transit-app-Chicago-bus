1003bf43f4ae1701464bc5d923b5fe0b873b840c
Ewelina <ekowalczyk1994@gmail.com>
2022-04-19 10:27:10 -0500

    new routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
+
+  private
+
+  def render_not_found_response
+    render json: {error: "Record not found"}, status: :not_found
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
 
   resources :stops, only: [:index]
+
+  resources :lines, only: [:index, :show]
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
