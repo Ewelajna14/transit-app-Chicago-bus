f8c7f60648bcdb168745b830c46490a1783b9275
Ewelina <ekowalczyk1994@gmail.com>
2022-04-25 09:13:47 -0500

    stops were added

--- client/src/Components/Stops.js
+++ client/src/Components/Stops.js
@@ -1,20 +1,20 @@
 
 import { useSelector} from "react-redux";
+import Stop from "./Stop"
 
 function Stops(){
 
-    
-    
+    const stops = useSelector((state)=>state.stops.entities["bustime-response"].stops)
 
-    const stops = useSelector((state)=>state.stops.entities)
-
-    console.log(stops)
+    const stopArray = stops.map((stop)=>{
+    return(<Stop key={stop.stpid} stop={stop}/>)
+    })
 
 
     return(
-        <div>
-
-        </div>
+        <ul>
+         {stopArray}
+        </ul>
     )
 }
 
