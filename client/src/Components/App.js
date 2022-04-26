911f187a6d409d6dd25565ba68ee0d0d76cf780f
Ewelina <ekowalczyk1994@gmail.com>
2022-04-26 10:41:13 -0500

    stops details

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,10 +1,10 @@
 
 import { Route, Routes} from "react-router-dom";
-
 import NavBar from './NavBar';
 import Schedule from './Schedule';
 import LineDetails from './LineDetails';
 import Stops from "./Stops";
+import StopDetail from "./StopDetail";
 
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
       <Route exact path ="/schedule" element={<Schedule/>}/>
       <Route exact path ="/details" element={<LineDetails/>}/>
       <Route exact path ="/stops" element={<Stops/>}/>
+      <Route exact path ="/stopDetail" element={<StopDetail/>}/>
     </Routes>
     </div>
   );
