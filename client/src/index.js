f8bb25feab7b334d8ac5d61aa9fea82b6d445327
Ewelina <ekowalczyk1994@gmail.com>
2022-04-26 11:28:42 -0500

    re-fetch changes

--- client/src/index.js
+++ client/src/index.js
@@ -7,14 +7,19 @@ import {CssBaseline} from "@mui/material"
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux"
 import store from "./redux/store"
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <CssBaseline/>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
     </Provider>
     </BrowserRouter>
   </React.StrictMode>,
