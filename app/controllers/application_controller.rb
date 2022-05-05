38286f07e6625bad98656b185efb5fccc603fe9b
Ewelina <ekowalczyk1994@gmail.com>
2022-05-05 16:43:19 -0500

    Sign up is working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,18 @@ class ApplicationController < ActionController::API
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
 
 
   private
 
   def render_not_found_response
-    render json: {error: "Record not found"}, status: :not_found
+    render json: {error: ["Record not found"] }, status: :not_found
+  end
+
+  def render_unprocessable_entity(exception)
+    render json: {errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
 
   resources :vehicles, only: [:show]
 
+  resources :users, only: [:create, :show]
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   
   
   # Routing logic: fallback requests for React Router.
