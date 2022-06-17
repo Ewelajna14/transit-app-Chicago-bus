cfa9b0f531377704bccb780caab5dcf3ba8a8ac9
Ewelina <ekowalczyk1994@gmail.com>
2022-06-17 08:52:48 -0500

    prepare for deploying app

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,7 +1,7 @@
 class StopsController < ApplicationController
     # GET/routes/:route_id/directions/direction_id/stops
     def index
-        url = "http://ctabustracker.com/bustime/api/v2/getstops?key=#{ENV["KEY"]}&rt=#{params[:route_id]}&dir=#{params[:direction_id]}&format=json"
+        url = "http://ctabustracker.com/bustime/api/v2/getstops?key=#{ENV["MY_API_KEY"]}&rt=#{params[:route_id]}&dir=#{params[:direction_id]}&format=json"
         response = RestClient.get(url)
         render json: response
     end 
